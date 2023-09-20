30b8ca5e29b3fe374ca7c3d7773c3deef2f1937a
Leonardo <martini.leonardo.ml@gmail.com>
2023-09-20 16:32:24 -0400

    refactoring

--- utils/plot.py
+++ utils/plot.py
@@ -24,26 +24,32 @@ class Plot():
     def __load_random_distribution__(self, file_name = "metrics_random_distribution.tsv"):
         return pd.read_csv(self.current_validation_path + file_name, sep = "\t", index_col = 0)
     
-    def run(self,dir_ = "drug_hub"):
+    def run(self,dir_ = "cosmic"):
         
         self.current_validation_path = self.validation_path + dir_ + "/"
         
         random_distribution_data_frame = self.__load_random_distribution__()
         metrics_df = self.__load_metrics__()
-        f,axes = plt.subplots(1,3,figsize = (16,5))
-
-        for index, disease in enumerate(random_distribution_data_frame["GWAS"].unique()):
-            
-            current_df = random_distribution_data_frame.loc[(random_distribution_data_frame['GWAS'] == disease)]
-            print(current_df)
-            current_metrics = metrics_df.loc[(metrics_df['GWAS'] == disease) ]
-            sns.boxplot(data = current_df,x="Algorithm", y="Recall",showfliers = False,color='white',ax = axes[index])
-            sns.scatterplot(data = current_metrics,x = "Algorithm", y = "Recall",s=200, color=".2", hue= "Algorithm", style = "Algorithm",ax= axes[index])
-
-        f.savefig('../imgs/prova.pdf',bbox_inches='tight')
+        f,axes = plt.subplots(3,3,figsize = (20,20))
+        diseases = list(random_distribution_data_frame["GWAS"].unique())
+        
+        for i in range(3):
+            for j in range(3):
+                index = 3*i + j
+                try:
+                    disease = diseases[index]
+                    current_df = random_distribution_data_frame.loc[(random_distribution_data_frame['GWAS'] == disease)]
+                    print(current_df)
+                    current_metrics = metrics_df.loc[(metrics_df['GWAS'] == disease) ]
+                    sns.boxplot(data = current_df,x="Algorithm", y="Recall",showfliers = False,color='white',ax = axes[i][j])
+                    sns.scatterplot(data = current_metrics,x = "Algorithm", y = "Recall",s=200, color=".2", hue= "Algorithm", style = "Algorithm",ax= axes[i][j])
+                    axes[i][j].set_title(disease)
+                except:
+                    pass
+        f.savefig('../imgs/' + dir_ + '.pdf',bbox_inches='tight')
 
 p = Plot(
-    experiment_dir_path = "../experiments/GWAS_study_exp/"
+    experiment_dir_path = "../experiments/algorithm_comparison/"
     )
 
 p.run()
